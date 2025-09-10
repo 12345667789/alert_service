e7f3c75b9ce3943acb67a31941306d06ec7d700c
Simon Puleo <Simon.Puleo@gmail.com>
2025-09-09 22:55:14 -0500

    Test automated deployment to dispatcher service

--- main.py
+++ main.py
@@ -319,3 +319,4 @@ if __name__ == "__main__":
     # For local development
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
+# Deployment test - Tue Sep  9 22:55:01 CDT 2025
